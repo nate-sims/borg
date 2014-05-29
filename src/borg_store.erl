4f38e2539a217d9fe01ea05105c042f2c5530de1
Nate Sims <nate_sims@coraid.com>
2014-05-29 12:22:06 -0700

    Commit

---
+++ src/borg_store.erl
@@ -0,0 +1,31 @@
+-module(borg_store).
+
+%% Module Functions
+-export([
+	 init/0
+	 ]).
+
+%% Store Functions
+-export([
+	 add/1,
+	 delete/1,
+	 get/1,
+	 update/2
+	 ]).
+
+
+init() ->
+    ok.
+
+add(_Node) ->
+   ok.
+
+delete(_Node) -> 
+    ok.
+
+get(_Node) ->
+    ok.
+
+update(_Node, _Attrs) -> 
+    ok.
+
